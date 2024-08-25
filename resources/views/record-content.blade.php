f474fc3963691ea489236beea70916562d40c231
Pim Dröge <pim@improvit.nl>
2024-08-25 20:02:51 +0200

    composer upgrade

--- resources/views/record-content.blade.php
+++ resources/views/record-content.blade.php
@@ -1,4 +1,18 @@
 {{-- Injected variables $record, $styles --}}
-<p>
-    {{ $record['title'] }}
-</p>
+@isset($record['title'])
+    <div class="card-header">
+        <div class="card-title">
+            <h5>{!! $record['title'] !!}</h5>
+        </div>
+    </div>
+@endisset
+@isset($record['content'])
+    <div class="card-body">
+        {!! $record['content'] !!}
+    </div>
+@endisset
+@isset($record['footer'])
+    <div class="card-footer">
+        {!! $record['footer'] !!}
+    </div>
+@endisset
