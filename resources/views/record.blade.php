e682cf1b8b1cda1ab07f587006e1ef8bce983a39
Pim Dröge <pim@improvit.nl>
2024-08-25 20:28:01 +0200

    composer upgrade

--- resources/views/record.blade.php
+++ resources/views/record.blade.php
@@ -4,6 +4,9 @@
      @if ($recordClickEnabled) wire:click="onRecordClick('{{ $record['id'] }}')" @endif
      class="{{ $styles['record'] }}">
 
+    @isset($record['ribbon'])
+        <div class="ribbon ribbon-info ribbon-shape">{!! $record['ribbon'] !!}</div>
+    @endisset
     @include($recordContentView, [
         'record' => $record,
         'styles' => $styles,
